28f2963db066bf16f3c517ffed65a7f2e76bebde
Piotr <piotr.ostrowski@aol.com>
2022-05-04 14:20:44 +0200

    get the base down

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellow: '#fffc52',
+      },
+    },
   },
   plugins: [],
 };
